96da411a8718f7330dcad80c5d822f0841aa3202
samuelwayoro <samuelwayoro@gmail.com>
2024-04-12 16:58:48 +0200

    first commit

---
+++ src/main/java/org/samydevup/mysqlcruddockerdemo/MysqlcruddockerdemoApplication.java
@@ -0,0 +1,13 @@
+package org.samydevup.mysqlcruddockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlcruddockerdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlcruddockerdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/samydevup/mysqlcruddockerdemo/MysqlcruddockerdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.samydevup.mysqlcruddockerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlcruddockerdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
